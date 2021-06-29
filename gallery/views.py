17513356beae662c1057008c1d4c26e9e3b1d897
JessyeTu <jt741@cam.ac.uk>
2021-06-29 19:31:17 +0100

    sorted gallery

--- gallery/views.py
+++ gallery/views.py
@@ -3,7 +3,7 @@ from .models import NewAlbum, NewAlbumPhoto
 # Create your views here.
 
 def gallery(request):
-    albums = NewAlbum.objects.filter(show_on_website=True)
+    albums = NewAlbum.objects.filter(show_on_website=True).order_by('-pub_date')
     photos = NewAlbumPhoto.objects.all()
     return render(request, 'gallery/gallery.html', {"albums": albums, "photos": photos})
 
